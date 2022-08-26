accf364805ff99db76cf44456e2a07f1777f04f3
geonryulPark <geonryul0131@gmail.com>
2022-08-26 22:58:40 +0900

    layout

---
+++ AirQuality(layout_permission)/app/src/main/java/com/eos/airqualitylayout_permission/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.eos.airqualitylayout_permission
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+// https://www.iqair.com/ko/ -> API key 받기
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
