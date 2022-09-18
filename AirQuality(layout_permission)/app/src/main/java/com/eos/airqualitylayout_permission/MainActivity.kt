600b0c5dbdc530e75cf2628c9e21bd60ad51c4b5
geonryulPark <geonryul0131@gmail.com>
2022-09-18 15:54:38 +0900

    no action, gradle

--- AirQuality(layout_permission)/app/src/main/java/com/eos/airqualitylayout_permission/MainActivity.kt
+++ AirQuality(layout_permission)/app/src/main/java/com/eos/airqualitylayout_permission/MainActivity.kt
@@ -4,7 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 // https://www.iqair.com/ko/ -> API key 받기
-
+// no action bar
+//
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
