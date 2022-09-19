453cae3ec58969f58a7606b5c7fdf0aa9fe66b3b
geonryulPark <geonryul0131@gmail.com>
2022-09-19 10:32:11 +0900

    API 설명 추가

--- AirQuality(layout_permission)/app/src/main/java/com/eos/airqualitylayout_permission/MainActivity.kt
+++ AirQuality(layout_permission)/app/src/main/java/com/eos/airqualitylayout_permission/MainActivity.kt
@@ -4,6 +4,15 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 // https://www.iqair.com/ko/ -> API key 받기
+/*
+1. 개인 대시보드
+2. 상단 메뉴에서 API 탭 클릭
+3. new key
+4. Community 로 선택 후, create 하면 완성
+
+Key 복사 후 저장하기
+
+ */
 // no action bar
 //
 
